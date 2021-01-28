7eed1b6c6651b2a18ad705faed345e0a495016c2
ShootGan <nawelpalepka@gmail.com>
2021-01-28 17:44:49 +0100

    komenda jest zrobiona

--- bot.py
+++ bot.py
@@ -1,11 +1,16 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix='.')
+client = commands.Bot(command_prefix='!')
 
 @client.event  ## start
 async def on_ready():
     print('bot is ready.')
+
+
+@client.command()  ##wpisanie danych
+async def sesja(ctx,*,x):
+     await ctx.send(x)
 
 def read_token():
     with open("token.txt",'r') as f:
@@ -13,5 +18,3 @@ def read_token():
         return lines[0].strip()
 TOKEN= read_token()
 client.run(TOKEN)
-
-
