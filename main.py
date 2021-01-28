99d2afe5f3cf80aad1d880f4cf22c5e1892ab16d
ShootGan <nawelpalepka@gmail.com>
2021-01-28 23:10:15 +0100

    ImportError: cannot import name 'read_token' from 'token_r' :((((

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from discord.ext import commands
 from token_r import start_functions11
 from cogs.reminder import Reminder
 client = commands.Bot(command_prefix='!')
-TOKEN= start_functions.read_token()
+TOKEN= start_functions11.read_token()
 if __name__ == '__main__':
-    start_functions.create_table()
+    start_functions11.create_table()
     client.add_cog(Reminder(client))
     client.run(TOKEN)
