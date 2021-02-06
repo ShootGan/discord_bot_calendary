3a04c14af4f94350aec1b0e1d577c6e147c4200c
ShootGan <nawelpalepka@gmail.com>
2021-02-06 23:49:51 +0100

    dsd

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from cogs.reminder import Reminder
 
 bot = commands.Bot(command_prefix='!')
 TOKEN = os.getenv('KEY')
-print(TOKEN)
+
 if __name__ == '__main__':
     bot.add_cog(Reminder(bot))
     bot.run(TOKEN)
